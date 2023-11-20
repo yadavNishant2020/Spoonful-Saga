375d5820dbc50c589a1dc14fd3c593d42eec7939
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-11-21 00:05:14 +0530

    Add dotenv configuration and error handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
+import "dotenv/config";
 import express from "express"
 import cors from "cors"
 import * as RecipeAPI from "./recipe-api"
+
 
 const app = express();
 const port = 3000;
@@ -9,8 +11,10 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/api/recipes/search", async (req, res) => {
+    // https://api.spoonacular.com/recipes/serach?searchTerm=burger&page=2
     const searchTerm = req.query.searchTerm as string;
     const page = parseInt(req.query.page as string);
+
     const result = await RecipeAPI.searchRecipes(searchTerm,page);
 
     return res.json(result);
--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -1,8 +1,15 @@
+require('dotenv').config();
 const apiKey = process.env.API_KEY;
+// console.log(apiKey);
+
 
 export const searchRecipes = async (searchTerm:string, page:number)=>{
 if(!apiKey){
     throw new Error("API key not found.")
+}
+
+if (typeof searchTerm !== "string" || typeof page !== "number") {
+    throw new Error("Invalid parameters.");
 }
 
 const url = new URL("https://api.spoonacular.com/recipes/complexSearch");
@@ -13,13 +20,14 @@ const queryParams = {
     number: "10",
     offset:(page * 10).toString()
 }
- url.search = new URLSearchParams(queryParams).toString()
+
+ url.search = new URLSearchParams(queryParams).toString() //Attach each value pairs to the url
 
  try {
     const searchRes = await fetch(url);
     const resultJSON = await searchRes.json();
     return resultJSON;
- } catch (error) {
-    console.log(error);
- }
+ } catch (error: any) {
+    throw new Error(`Error fetching recipes: ${(error as Error).message}`);
+}
 };
