ce62cb79bdff93812b6d164e9775132fc2c39fdb
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2024-01-09 03:00:31 +0530

    Background fixed

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,7 +2,7 @@ import Home from './components/Home'
 import RecipeSearchFunctionality from './components/RecipeSearchFunctionality'
 function App() {
     return (
-        <div className="bg-fixed bg-center bg-no-repeat bg-home-background md:bg-cover">
+        <div className="bg-fixed bg-center bg-no-repeat bg-[url('img/background.jpeg')] md:bg-cover">
             <Home />
             <RecipeSearchFunctionality />
         </div>
